ee1ca0579ff9403df6ec6a7e572c99baf2b3c01c
marcodeiulio <mdeiulio@icloud.com>
2022-02-08 16:14:04 +0100

    - Added Header.vue - Added .scss files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app" class="container">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+// TODO Dividere stile nei vari fogli scss
+@import url("./assets/sass/style.scss");
+
+* {
+  font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1000px;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <figure>
+      <img src="../assets/img/dc-logo.png" alt="DC Logo" />
+    </figure>
+    <nav>
+      <ul>
+        <li><a href="#">characters</a></li>
+        <li><a href="#">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  color: black;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+ul {
+  list-style-type: none;
+  li {
+    display: inline-block;
+    padding: 0 10px;
+    a {
+      text-decoration: none;
+      padding: 30px 0;
+      &:hover {
+        color: #0282f9;
+      }
+    }
+  }
+}
+</style>
