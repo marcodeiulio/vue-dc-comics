69296571fdabaa171b0841c64cd7060d8d72da45
marcodeiulio <mdeiulio@icloud.com>
2022-02-09 16:24:12 +0100

    Main.vue - Added data for dynamic print of ComicCard.vue ComicCard.vue - Edited to print dynamically

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="comic-card">
     <figure>
-      <img
-        src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-        alt="IMG"
-      />
+      <img :src="thumb" :alt="series + ' Cover Image'" />
     </figure>
-    <h5>Action Comics</h5>
+    <h5>{{ series }}</h5>
   </div>
 </template>
 
 <script>
 export default {
   name: "ComicCard",
+  props: ["thumb", "series"],
 };
 </script>
 
